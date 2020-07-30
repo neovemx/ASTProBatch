dd3c9aee77d8c13f072f7482f1ef6a01ea9c1b5b
MGI-Accusys <MGI-Accusys@LAPTOP-D390CEPU>
2020-07-30 17:15:38 -0400

    Changes

---
+++ AST ProBatch Mobile/AST ProBatch Mobile.iOS/Renderers/StandardPickerRenderer.cs
@@ -0,0 +1,57 @@
+﻿using AST_ProBatch_Mobile.Controls;
+using AST_ProBatch_Mobile.iOS.Renderers;
+using System.ComponentModel;
+using System.Drawing;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(StandardPicker), typeof(StandardPickerRenderer))]
+namespace AST_ProBatch_Mobile.iOS.Renderers
+{
+    public class StandardPickerRenderer : PickerRenderer
+    {
+        public StandardPicker ElementV2 => Element as StandardPicker;
+        public UITextFieldPadding ControlV2 => Control as UITextFieldPadding;
+
+        protected override UITextField CreateNativeControl()
+        {
+            var control = new UITextFieldPadding(RectangleF.Empty)
+            {
+                Padding = ElementV2.Padding,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                ClipsToBounds = true
+            };
+
+            UpdateBackground(control);
+
+            return control;
+        }
+
+        protected void UpdateBackground(UITextField control)
+        {
+            if (control == null) return;
+            control.Layer.CornerRadius = ElementV2.CornerRadius;
+            control.Layer.BorderWidth = ElementV2.BorderThickness;
+            control.Layer.BorderColor = ElementV2.BorderColor.ToCGColor();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == StandardEntry.PaddingProperty.PropertyName)
+            {
+                UpdatePadding();
+            }
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        protected void UpdatePadding()
+        {
+            if (Control == null)
+                return;
+
+            ControlV2.Padding = ElementV2.Padding;
+        }
+    }
+}
