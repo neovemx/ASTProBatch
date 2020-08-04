ada133e1fb5ae214577882e1c0a5cc972e268eb4
neovemx@hotmail.com <F3anc12c0>
2020-08-04 18:27:45 -0400

    Add from Mac

---
+++ AST ProBatch Mobile/AST ProBatch Mobile/Utilities/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Acr.UserDialogs;
+
+namespace AST_ProBatch_Mobile.Utilities
+{
+    public class Alert
+    {
+        public static async void Show(string message)
+        {
+            await UserDialogs.Instance.AlertAsync(message, "AST●ProBatch®", "Cerrar");
+        }
+
+        public static async void Show(string message, string buttontext)
+        {
+            await UserDialogs.Instance.AlertAsync(message, "AST●ProBatch®", buttontext);
+        }
+    }
+
+    public class Confirm
+    {
+        public static Task<bool> Show(string message)
+        {
+            return UserDialogs.Instance.ConfirmAsync(message, "AST●ProBatch®", "Si", "No");
+        }
+
+        public static Task<bool> Show(string message, string okbuttonmessage, string cancelbuttonmessage)
+        {
+            return UserDialogs.Instance.ConfirmAsync(message, "AST●ProBatch®", okbuttonmessage, cancelbuttonmessage);
+        }
+    }
+
+    public static class StatusColor
+    {
+        public static string Green { get { return "#33CC33"; } }
+        public static string Red { get { return "#FE6347"; } }
+        public static string Orange { get { return "#FDA600"; } }
+        public static string Yellow { get { return "#FFFF00"; } }
+        public static string White { get { return "#FFFFFF"; } }
+        public static string Grey { get { return "#7F7F7F"; } }
+        public static string Purple { get { return "#B9908E"; } }
+        public static string Blue { get { return "#87CEFA"; } }
+    }
+}
---
+++ AST ProBatch Mobile/AST ProBatch Mobile/ViewModels/MonitoringAndExecution/Execution/ControlSchedulesExecutionDetailViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Input;
+using AST_ProBatch_Mobile.Models;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+
+namespace AST_ProBatch_Mobile.ViewModels
+{
+    public class ControlSchedulesExecutionDetailViewModel : BaseViewModel
+    {
+        #region Atributes
+        private CommandsToControl commandtocontrol;
+        #endregion
+
+        #region Properties
+        public CommandsToControl CommandToControl
+        {
+            get { return commandtocontrol; }
+            set { SetValue(ref commandtocontrol, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public ControlSchedulesExecutionDetailViewModel(CommandsToControl commandToControl)
+        {
+            this.CommandToControl = commandToControl;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand CloseCommand
+        {
+            get
+            {
+                return new RelayCommand(Close);
+            }
+        }
+
+        private async void Close()
+        {
+            await Application.Current.MainPage.Navigation.PopModalAsync();
+        }
+        #endregion
+
+        #region Helpers
+
+        #endregion
+    }
+}
